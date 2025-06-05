52f2336bcf80d23120958ae45901ac88a7ce158d
Sherest Mehta <svfranklindemo@gmail.com>
2025-06-05 15:59:55 +0530

    fixing null pointer

--- blocks/cf-carousel/cf-carousel.js
+++ blocks/cf-carousel/cf-carousel.js
@@ -132,7 +132,7 @@ export default function decorate(block) {
   arrowContainer.append(leftArrow, rightArrow);
 
   if (window.innerWidth >= 1024) {
-    arrowContainer.style.width = `${cardMaxWidth * slidesToShowNow + (slidesToShowNow - 1) * 20 + 10}px`;
+    arrowContainer.style.width = `${320 * slidesToShowNow + (slidesToShowNow - 1) * 20 + 10}px`;
   }
 
   let currentPage = 0;
