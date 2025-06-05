0b47b2c7a04919744df7add32bd09da141c2929b
Sherest Mehta <svfranklindemo@gmail.com>
2025-06-05 15:31:53 +0530

    updated icons

--- blocks/cf-carousel/cf-carousel.js
+++ blocks/cf-carousel/cf-carousel.js
@@ -110,12 +110,20 @@ export default function decorate(block) {
   // Create left and right arrow buttons
   const leftArrow = document.createElement('button');
   leftArrow.classList.add('cf-carousel-arrow', 'cf-carousel-arrow-left');
-  leftArrow.innerHTML = '&#8592;'; // Left arrow symbol
+  leftArrow.innerHTML = `
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M15.5 19L9.5 12L15.5 5" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  `;
   leftArrow.style.display = 'none';
 
   const rightArrow = document.createElement('button');
   rightArrow.classList.add('cf-carousel-arrow', 'cf-carousel-arrow-right');
-  rightArrow.innerHTML = '&#8594;'; // Right arrow symbol
+  rightArrow.innerHTML = `
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M8.5 5L14.5 12L8.5 19" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  `;
   rightArrow.style.display = 'none';
 
   let currentPage = 0;
